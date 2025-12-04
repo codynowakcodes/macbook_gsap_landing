40b3268d4de2f64d4bc228b123bc69d2a1f6ee40
Cody Nowak <cody.nowak@yahoo.com>
2025-12-04 13:55:15 -0500

    implement features section

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const navLinks = [
+const navLinks = [
     {label: 'Store' },
     {label: 'Mac' },
     {label: 'iPhone' },
@@ -7,7 +7,7 @@ export const navLinks = [
     {label: 'AirPods' },
 ];
 
-export const noChangeParts = [
+const noChangeParts = [
     "Object_84",
     "Object_37",
     "Object_34",
@@ -25,7 +25,7 @@ export const noChangeParts = [
     "Object_10",
 ]
 
-export const performanceImages = [
+const performanceImages = [
     { id: "p1", src: "/performance1.png"},
     { id: "p2", src: "/performance2.png"},
     { id: "p3", src: "/performance3.png"},
@@ -35,7 +35,7 @@ export const performanceImages = [
     { id: "p7", src: "/performance7.png"},
 ]
 
-export const performanceImgPositions = [
+const performanceImgPositions = [
     {
         id: "p1",
         left: 5,
@@ -71,4 +71,60 @@ export const performanceImgPositions = [
         left: -5,
         bottom: 0,
     },
-]
+];
+
+const features = [
+    {
+        id: 1,
+        icon: "/feature-icon1.svg",
+        highlight: "Email AI.",
+        text:"Summarize and draft replies to emails instantly, so you stay on top of your inbox.",
+        styles: "left-5 md:left-20 top-[20%] opacity-0 translate-y-5",
+    },
+    {
+        id: 2,
+        icon: "/feature-icon2.svg",
+        highlight: "Image AI.",
+        text:"Generate or edit images with ease. Just type what you imagine, and let AI bring it to life.",
+        styles: "right-5 md:right-20 top-[30%] opacity-0 translate-y-5",
+    },
+    {
+        id: 3,
+        icon: "/feature-icon3.svg",
+        highlight: "Summarize AI.",
+        text:"Turn long articles, reports, or notes into clear, bite-sized summaries in seconds.",
+        styles: "left-5 md:left-20 top-[50%] opacity-0 translate-y-5",
+    },
+    {
+        id: 4,
+        icon: "/feature-icon4.svg",
+        highlight: "AirDrop.",
+        text:"Wirelessly share photos, large files, and more between your iPhone, your Mac, & other devices.",
+        styles: "right-5 md:right-20 top-[70%] opacity-0 translate-y-5",
+    },
+    {
+        id: 1,
+        icon: "/feature-icon5.svg",
+        highlight: "Writing Tool.",
+        text:"Write smarter and faster, whether it's blogs, essays, or captions, AI helps polish your words.",
+        styles: "left-5 md:left-20 top-[90%] opacity-0 translate-y-5",
+    },
+];
+
+export const featureSequence = [
+    {videoPath: "/videos/feature-1.mp4", boxClass: ".box1", delay: 1},
+    {videoPath: "/videos/feature-2.mp4", boxClass: ".box2", delay: 0},
+    {videoPath: "/videos/feature-3.mp4", boxClass: ".box3", delay: 0},
+    {videoPath: "/videos/feature-4.mp4", boxClass: ".box4", delay: 0},
+    {videoPath: "/videos/feature-5.mp4", boxClass: ".box5", delay: 0},
+];
+
+export {
+    features,
+    // featureSequences,
+    // footerLinks,
+    navLinks,
+    noChangeParts,
+    performanceImages,
+    performanceImgPositions,
+}
